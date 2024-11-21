2355aec61a95b13b9f5ceba215ececaa241b709b
Abdullah Al Suad <suadabdullahal@gmail.com>
2024-11-21 18:24:10 +0600

    up

--- impo/stringtovowelsfind.cpp
+++ impo/stringtovowelsfind.cpp
@@ -12,24 +12,24 @@ int main()
         cin >> arr[i];
     }
 
-    int count = 0;
-    // int index = 0;
-    // char vowels[index];
-    int vowels[count];
+    // int count = 0;
+    int index = 0;
+    char vowels[index];
+    // int vowels[count];
     for (int i = 0; i < N; i++)
     {
         if (arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' ||
             arr[i] == 'O' || arr[i] == 'U' || arr[i] == 'a' ||
             arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u')
         {
-            count += 1;
-            vowels[i] = arr[i];
-            // vowels[index++] = arr[i];
+            // count += 1;
+            // vowels[i] = arr[i];
+            vowels[index++] = arr[i];
         }
     }
     // cout << "count" << count;
 
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < index; i++)
     {
         cout << vowels[i];
     }
